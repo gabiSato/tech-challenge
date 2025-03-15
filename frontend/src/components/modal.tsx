ad9cb4a30a159fd5a683fe9acb72185ceb5ab4e8
Gabriela Sato <gabisato.ssb@gmail.com>
2025-03-15 18:35:21 -0300

    creates modall component

---
+++ frontend/src/components/modal.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React, { useEffect, useState } from "react";
+
+import CloseIcon from "../../public/icons/close.svg";
+import clsx from "clsx";
+
+interface ModalProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+  children: React.ReactNode;
+}
+
+export default function Modal({ open, onOpenChange, children }: ModalProps) {
+  useEffect(() => {
+    document.body.style.overflow = open ? "hidden" : "";
+
+    return () => {
+      document.body.style.overflow = "";
+    };
+  }, [open]);
+
+  function closeModal() {
+    onOpenChange(false);
+  }
+
+  return (
+    <div
+      className={clsx("fixed top-0 left-0 z-[1300] w-screen h-dvh", {
+        invisible: !open,
+      })}
+    >
+      <div className="fixed inset-0 bg-black/60" onClick={closeModal}></div>
+
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white min-w-[400px] min-h-48 overflow-y-auto">
+        <button
+          className="absolute top-16 right-16 cursor-pointer"
+          onClick={closeModal}
+        >
+          <CloseIcon className="h-16" />
+        </button>
+
+        {children}
+      </div>
+    </div>
+  );
+}
